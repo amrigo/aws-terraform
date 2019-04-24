c3db9641e05f44327940d6b36e9fc202e5f98686
Bruno Tougeiro <b.tougeiro@icloud.com>
2019-04-24 15:30:08 +0100

    changed string to var.ami

--- exercise_2/ami_copy.tf
+++ exercise_2/ami_copy.tf
@@ -1,12 +1,12 @@
 resource "aws_ami_copy" "ami-copy" {
     name = "terraform-ami-encrypted-copy"
-    description = "A copy of ami-090f10efc254eaf55 encrypted"
+    description = "A copy of ${var.ami} encrypted"
     source_ami_id = "${var.ami}"
     source_ami_region = "${var.region}"
     encrypted = true
     kms_key_id = "${aws_kms_key.kms_key.arn}"
 
     tags {
-        Name = "ami-090f10efc254eaf55-copy"
+        Name = "${var.ami}-copy"
     }
 }
