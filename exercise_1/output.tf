4839adbccc4e07fb58c85fb7dec56269e36303dd
Bruno Tougeiro <b.tougeiro@icloud.com>
2019-04-24 16:14:02 +0100

    folder deleted

--- exercise_1/output.tf
+++
@@ -1,4 +0,0 @@
-output "public_dns" {
-  description = "Public DNS test instance"
-  value       = "${aws_instance.test.public_dns}"
-}
--- exercise_1/subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "public" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_blocks["subnet"]}"
-
-  tags {
-    Name = "test-subnet"
-  }
-}
