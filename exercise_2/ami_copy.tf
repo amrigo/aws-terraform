ea87e041050bf760a12226d461179165abc47300
Bruno Tougeiro <b.tougeiro@icloud.com>
2019-04-24 14:56:40 +0100

    file with vars

--- exercise_2/ami_copy.tf
+++ exercise_2/ami_copy.tf
@@ -1,8 +1,8 @@
 resource "aws_ami_copy" "ami-copy" {
     name = "terraform-ami-encrypted-copy"
     description = "A copy of ami-090f10efc254eaf55"
-    source_ami_id = "ami-090f10efc254eaf55"
-    source_ami_region = "eu-central-1"
+    source_ami_id = "${var.ami}"
+    source_ami_region = "${var.region}"
     encrypted = true
 
     tags {
